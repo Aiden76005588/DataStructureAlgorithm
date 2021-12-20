939235e73da57860662a44fd6eec750ea2f4ca22
Aiden <66179677+Aiden76005588@users.noreply.github.com>
2021-12-20 22:50:15 +0900

    [Array2]

--- 1.Array.js
+++ 1.Array.js
@@ -13,3 +13,25 @@ console.log(arr3);
 // 특정 로직을 사용하여 초기화할 경우 from을 사용할 수 있다.
 let arr4 = Array.from({ length: 100 }, (_, i) => i);
 console.log(arr4);
+
+//자바스크립트의 Array는 다른 언어의 Array와 조금 다르다
+//자바스크립트의 Array는 동적이다.
+
+const arr = [];
+console.log(arr);
+arr.push(1);
+arr.push(2);
+arr.push(3);
+arr.push(4);
+console.log(arr);
+
+//자바스크립트의 Array는 HashMap에 가깝다.
+console.log(arr.length);
+
+//index가 number가 아니어도 된다.
+arr["string"] = 10;
+arr[false] = 0;
+console.log(arr);
+console.log(arr.length);
+arr[4] = 5;
+console.log(arr.length);
